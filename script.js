6034e659cc6372696ffa751fcd109bd082fd7399
Snehdeep Singh <snehdeepsingh221199@gmail.com>
2021-01-05 20:36:46 +0530

    first commit (testing)

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('#container');
+
+const div = document.createElement('div');
+div.setAttribute('style','background: blue; width: 20px; height: 20px; position: relative; padding: 0'); 
