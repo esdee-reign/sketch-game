e1f721fc69368b97db286f36770e4f3e378b623f
Snehdeep Singh <snehdeepsingh221199@gmail.com>
2021-01-10 00:30:48 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-const container = document.querySelector('#container');
+const grid = document.querySelector('.grid');
 
-const div = document.createElement('div');
-div.setAttribute('style','background: blue; width: 20px; height: 20px; position: relative; padding: 0'); 
+function createGrid() {
+    for(let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = 'black';
+        });
+        grid.appendChild(div);
+    }
+};
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const slider = document.querySelector('#slider');
+const screenVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    screenVal.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for(let i = 0; i < val*val; i++){
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = "black";
+        })
+        grid.appendChild(div);
+    }
+})
+
+createGrid();
